be626a52d61b0f1424bb2605a13baad137be3ba0
Joe-LB-03 <louis.bishop@live.co.uk>
2023-03-23 12:47:44 +0000

    basic solution

--- src/main/java/Baccarat.java
+++ src/main/java/Baccarat.java
@@ -1,3 +1,29 @@
-public class Baccarat {
-  // TODO: Implement your Baccarat simulation program here
+public class Baccarat 
+{
+    public static void main(String[] args)
+    {
+        Shoe shoe = new Shoe(6);
+        shoe.shuffle();
+
+        BaccaratHand playerHand = new BaccaratHand();
+        BaccaratHand bankerHand = new BaccaratHand();
+
+        for(int cards = 0; cards < 2; cards++)
+        {
+            playerHand.add(shoe.deal());
+            bankerHand.add(shoe.deal());
+        }
+
+        System.out.println("Player: " + playerHand.toString() + " = " + playerHand.value());
+        System.out.println("Banker: " + bankerHand.toString() + " = " + bankerHand.value());
+
+        if(playerHand.isNatural())
+        {
+            System.out.println("Player has a Natural\n");
+        }
+        if(bankerHand.isNatural())
+        {
+            System.out.println("Banker has a Natural\n");
+        }
+    }
 }
